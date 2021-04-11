c052b97b9ddbe79b170313626b31627790bd137b
Truong Le <truong0996@gmail.com>
2021-04-11 12:11:02 +0700

    update show role priv

--- proc_show_user.sql
+++ proc_show_user.sql
@@ -4,14 +4,12 @@ is
 begin
     open c1 for select * from dba_users;
 end;
-
 --show user privillage system
 create or replace procedure showUserPrivSys (username in VARCHAR2, c1 out sys_refcursor)
 is
 begin
     open c1 for select * from dba_sys_privs where GRANTEE = username;
 end;
-
 --show user privillage table
 create or replace procedure showUserPrivTab (username in VARCHAR2, c1 out sys_refcursor)
 is
@@ -19,6 +17,17 @@ begin
     open c1 for select table_name, privilege, grantable from dba_tab_privs where GRANTEE = username;
 end;
 
+--show role privillage system 
+create or replace procedure showRolePrivSys (rolename in varchar2, c1 out sys_refcursor)
+is
+begin
+    open c1 for select * from role_sys_privs where ROLE = rolename;
+end;
 
-
+--show role privillage table 
+create or replace procedure showRolePrivTab (rolename in varchar2, c1 out sys_refcursor)
+is
+begin
+    open c1 for select * from role_tab_privs where ROLE = rolename;
+end;
 
